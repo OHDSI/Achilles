c2081221bb5d64bdafba0a7336d78c74dd7a7cb9
unknown <mschuemi@WPRDUSMJTGLAY.wks.jnj.com>
2014-05-22 02:55:56 -0400

    Achilles now runs on Oracle (albeit very slowly)

--- R/TestCode.R
+++ R/TestCode.R
@@ -4,22 +4,10 @@ testCode <- function(){
   setwd("c:/temp")
   connectionDetails <- createConnectionDetails(dbms="sql server", server="RNDUSRDHIT07.jnj.com")
   achillesResults <- achilles(connectionDetails, cdmSchema="cdm4_sim", resultsSchema="scratch")
-  write.table(achillesResults$sql, file="c:/temp/ach.sql")
-  
-  
-  write.table(error$sql,"c:/temp/problem.sql")
-  
-  
-  
-  
-  sql <- read.table("c:/temp/rendered.sql")
-  y <- splitSql(as.character(sql$x))
-  for (i in 1:length(y)){
-    write.table(y[i], file = paste("c:/temp/sql_",i,".sql",sep=""))
-  }
-  
-  
-  sql <- read.table("c:/temp/sql_28.sql")
-  y <- splitSql(as.character(sql$x))
-  str(y)
+
+
+  setwd("c:/temp")
+  connectionDetails <- createConnectionDetails(dbms="oracle", server="xe", user="system",password="xxx")
+  achillesResults <- achilles(connectionDetails, cdmSchema="cdm4_sim", resultsSchema="scratch")
+
 }
