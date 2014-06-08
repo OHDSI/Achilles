acaff294243f771d11c250134319318559eb6226
schuemie <schuemie@ohdsi.org>
2014-06-08 16:50:09 -0700

    Updated documentation

---
+++ R/Achilles-package.R
@@ -0,0 +1,26 @@
+#' Achilles
+#'
+#' @name Achilles
+#' @docType package
+NULL
+
+#' Achilles analyses details
+#' 
+#' A data frame describing all Achilles analyses and their IDs. The variables are as follows:
+#'
+#' \itemize{
+#'   \item ANALYSIS_ID 
+#'   \item ANALYSIS_NAME
+#'   \item STRATUM_1_NAME
+#'   \item STRATUM_2_NAME
+#'   \item STRATUM_3_NAME
+#'   \item STRATUM_4_NAME
+#'   \item STRATUM_5_NAME
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name analysesDetails
+#' @usage data(analysesDetails)
+#' @format A data frame with 7 columns
+NULL
