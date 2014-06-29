f5e8d2d68d40037091b90932996ede7a2035bc38
toanong <toanch@gmail.com>
2014-06-29 06:52:46 -0600

    Fix sqlAgeAtFirstDiagnosis
    Missing CAST function

--- inst/sql/sql_server/export/conditionera/sqlAgeAtFirstDiagnosis.sql
+++ inst/sql/sql_server/export/conditionera/sqlAgeAtFirstDiagnosis.sql
@@ -9,8 +9,8 @@ select c1.concept_id as concept_id,
 	ard1.max_value as max_value
 from ACHILLES_results_dist ard1
 	inner join @cdmSchema.dbo.concept c1
-	on ard1.stratum_1 = c1.concept_id
+	on CAST(ard1.stratum_1 AS INT) = c1.concept_id
 	inner join @cdmSchema.dbo.concept c2
-	on ard1.stratum_2 = c2.concept_id
+	on CAST(ard1.stratum_2 AS INT) = c2.concept_id
 where ard1.analysis_id = 1006
 and ard1.count_value > 0
