3c0241f602acb28a427a26c5e3cb122afe010c76
schuemie <schuemie@ohdsi.org>
2014-08-08 08:31:36 -0400

    Fixed overflow on Redshift

--- R/TestExportCode.R
+++ R/TestExportCode.R
@@ -8,13 +8,13 @@ testExportCode <- function(){
   setwd("c:/temp")
   connectionDetails <- createConnectionDetails(dbms="sql server", server="RNDUSRDHIT07.jnj.com")
   exportToJson(connectionDetails, cdmSchema = "cdm4_sim", resultsSchema = "scratch",outputPath = "c:/temp/SqlServer")
-  #exportToJson(connectionDetails, cdmSchema = "cdm4_sim", resultsSchema = "scratch",outputPath = "c:/temp/SqlServer",reports = c("PROCEDURE"))
+  #exportToJson(connectionDetails, cdmSchema = "cdm4_sim", resultsSchema = "scratch",outputPath = "c:/temp/SqlServer",reports = c("CONDITION_ERA"))
   
   #Test on PostgreSQL
   setwd("c:/temp")
   connectionDetails <- createConnectionDetails(dbms="postgresql", server="localhost/ohdsi", user="postgres",password=pw)
   exportToJson(connectionDetails, cdmSchema = "cdm4_sim", resultsSchema = "scratch",outputPath = "c:/temp/PostgreSQL")
-  #exportToJson(connectionDetails, cdmSchema = "cdm4_sim", resultsSchema = "scratch",outputPath = "c:/temp/PostgreSQL", reports = c("DRUG"))
+  #exportToJson(connectionDetails, cdmSchema = "cdm4_sim", resultsSchema = "scratch",outputPath = "c:/temp/PostgreSQL", reports = c("CONDITION_ERA"))
   
   #Test on PostgreSQL sample
   setwd("c:/temp")
@@ -26,6 +26,7 @@ testExportCode <- function(){
   setwd("c:/temp")
   connectionDetails <- createConnectionDetails(dbms="oracle", server="xe", user="system",password=pw)
   exportToJson(connectionDetails, cdmSchema = "cdm4_sim", resultsSchema = "scratch",outputPath = "c:/temp/Oracle")  
+  #exportToJson(connectionDetails, cdmSchema = "cdm4_sim", resultsSchema = "scratch",outputPath = "c:/temp/Oracle", report = c("CONDITION_ERA")) 
   
   #Compare JSON files:
   loadTextFile <- function(fileName){
