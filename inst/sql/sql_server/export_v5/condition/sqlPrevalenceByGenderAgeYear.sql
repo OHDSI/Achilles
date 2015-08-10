OHDSI/Achilles

commit a18b3d2f45f577df7c3f18da8c1c570e4b66d310

Cast to VARCHAR over INT since any INT can cast into VARCHAR, but not every VARCHAR can cast into INT.

inst/sql/sql_server/export_v5/condition/sqlPrevalenceByGenderAgeYear.sql
@@ -1,14 +1,14 @@
 SELECT c1.concept_id AS concept_id,
-	c1.concept_name as concept_name,
+  c1.concept_name as concept_name,
 	cast(num_stratum_4 * 10 AS VARCHAR) + '-' + cast((num_stratum_4 + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
 	c2.concept_name AS series_name,  --gender
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons
 FROM (
-	SELECT CAST(num.stratum_1 AS INT) AS num_stratum_1,
-		CAST(num.stratum_2 AS INT) AS num_stratum_2,
-		CAST(num.stratum_3 AS INT) AS num_stratum_3,
-		CAST(num.stratum_4 AS INT) AS num_stratum_4,
+	SELECT num.stratum_1 AS num_stratum_1,
+		num.stratum_2 AS num_stratum_2,
+		num.stratum_3 AS num_stratum_3,
+		num.stratum_4 AS num_stratum_4,
 		num.count_value AS num_count_value,
 		denom.count_value AS denom_count_value
 	FROM (
@@ -28,8 +28,8 @@ FROM (
 			AND num.stratum_4 = denom.stratum_3
 	) tmp
 INNER JOIN @cdm_database_schema.concept c1
-	ON num_stratum_1 = c1.concept_id
+	ON num_stratum_1 = CAST(c1.concept_id as VARCHAR)
 INNER JOIN @cdm_database_schema.concept c2
-	ON num_stratum_3 = c2.concept_id
+	ON num_stratum_3 = CAST(c2.concept_id as VARCHAR)
 ORDER BY c1.concept_id,
 	num_stratum_2

inst/sql/sql_server/export_v5/conditionera/sqlPrevalenceByGenderAgeYear.sql
@@ -4,10 +4,10 @@ SELECT c1.concept_id AS concept_id,
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons
 FROM (
-	SELECT CAST(num.stratum_1 AS INT) AS num_stratum_1,
-		CAST(num.stratum_2 AS INT) AS num_stratum_2,
-		CAST(num.stratum_3 AS INT) AS num_stratum_3,
-		CAST(num.stratum_4 AS INT) AS num_stratum_4,
+	SELECT num.stratum_1 AS num_stratum_1,
+		num.stratum_2 AS num_stratum_2,
+		num.stratum_3 AS num_stratum_3,
+		num.stratum_4 AS num_stratum_4,
 		num.count_value AS num_count_value,
 		denom.count_value AS denom_count_value
 	FROM (
@@ -27,8 +27,8 @@ FROM (
 			AND num.stratum_4 = denom.stratum_3
 	) tmp
 INNER JOIN @cdm_database_schema.concept c1
-	ON num_stratum_1 = c1.concept_id
+	ON num_stratum_1 = CAST(c1.concept_id as VARCHAR)
 INNER JOIN @cdm_database_schema.concept c2
-	ON num_stratum_3 = c2.concept_id
+	ON num_stratum_3 = CAST(c2.concept_id as VARCHAR)
 ORDER BY c1.concept_id,
 	num_stratum_2

inst/sql/sql_server/export_v5/drug/sqlPrevalenceByGenderAgeYear.sql
@@ -5,10 +5,10 @@ SELECT c1.concept_id AS concept_id,
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons
 FROM (
-	SELECT CAST(num.stratum_1 AS INT) AS num_stratum_1,
-		CAST(num.stratum_2 AS INT) AS num_stratum_2,
-		CAST(num.stratum_3 AS INT) AS num_stratum_3,
-		CAST(num.stratum_4 AS INT) AS num_stratum_4,
+	SELECT num.stratum_1 AS num_stratum_1,
+		num.stratum_2 AS num_stratum_2,
+		num.stratum_3 AS num_stratum_3,
+		num.stratum_4 AS num_stratum_4,
 		num.count_value AS num_count_value,
 		denom.count_value AS denom_count_value
 	FROM (
@@ -28,8 +28,6 @@ FROM (
 			AND num.stratum_4 = denom.stratum_3
 	) tmp
 INNER JOIN @cdm_database_schema.concept c1
-	ON num_stratum_1 = c1.concept_id
+	ON num_stratum_1 = CAST(c1.concept_id as VARCHAR)
 INNER JOIN @cdm_database_schema.concept c2
-	ON num_stratum_3 = c2.concept_id
-ORDER BY c1.concept_id,
-	num_stratum_2
+	ON num_stratum_3 = CAST(c2.concept_id as VARCHAR)

inst/sql/sql_server/export_v5/drugera/sqlPrevalenceByGenderAgeYear.sql
@@ -4,10 +4,10 @@ SELECT c1.concept_id AS concept_id,
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons
 FROM (
-	SELECT CAST(num.stratum_1 AS INT) AS num_stratum_1,
-		CAST(num.stratum_2 AS INT) AS num_stratum_2,
-		CAST(num.stratum_3 AS INT) AS num_stratum_3,
-		CAST(num.stratum_4 AS INT) AS num_stratum_4,
+	SELECT num.stratum_1 AS num_stratum_1,
+		num.stratum_2 AS num_stratum_2,
+		num.stratum_3 AS num_stratum_3,
+		num.stratum_4 AS num_stratum_4,
 		num.count_value AS num_count_value,
 		denom.count_value AS denom_count_value
 	FROM (
@@ -27,8 +27,7 @@ FROM (
 			AND num.stratum_4 = denom.stratum_3
 	) tmp
 INNER JOIN @cdm_database_schema.concept c1
-	ON num_stratum_1 = c1.concept_id
+	ON num_stratum_1 = CAST(c1.concept_id as VARCHAR)
 INNER JOIN @cdm_database_schema.concept c2
-	ON num_stratum_3 = c2.concept_id
-ORDER BY c1.concept_id,
-	num_stratum_2
+	ON num_stratum_3 = CAST(c2.concept_id as VARCHAR)
+

inst/sql/sql_server/export_v5/observation/sqlPrevalenceByGenderAgeYear.sql
@@ -5,10 +5,10 @@ SELECT c1.concept_id AS concept_id,
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons
 FROM (
-	SELECT CAST(num.stratum_1 AS INT) AS num_stratum_1,
-		CAST(num.stratum_2 AS INT) AS num_stratum_2,
-		CAST(num.stratum_3 AS INT) AS num_stratum_3,
-		CAST(num.stratum_4 AS INT) AS num_stratum_4,
+	SELECT num.stratum_1 AS num_stratum_1,
+		num.stratum_2 AS num_stratum_2,
+		num.stratum_3 AS num_stratum_3,
+		num.stratum_4 AS num_stratum_4,
 		num.count_value AS num_count_value,
 		denom.count_value AS denom_count_value
 	FROM (
@@ -28,8 +28,7 @@ FROM (
 			AND num.stratum_4 = denom.stratum_3
 	) tmp
 INNER JOIN @cdm_database_schema.concept c1
-	ON num_stratum_1 = c1.concept_id
+	ON num_stratum_1 = CAST(c1.concept_id as VARCHAR)
 INNER JOIN @cdm_database_schema.concept c2
-	ON num_stratum_3 = c2.concept_id
-ORDER BY c1.concept_id,
-	num_stratum_2
+	ON num_stratum_3 = CAST(c2.concept_id as VARCHAR)
+

inst/sql/sql_server/export_v5/procedure/sqlPrevalenceByGenderAgeYear.sql
@@ -5,10 +5,10 @@ SELECT c1.concept_id AS concept_id,
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons
 FROM (
-	SELECT CAST(num.stratum_1 AS INT) AS num_stratum_1,
-		CAST(num.stratum_2 AS INT) AS num_stratum_2,
-		CAST(num.stratum_3 AS INT) AS num_stratum_3,
-		CAST(num.stratum_4 AS INT) AS num_stratum_4,
+	SELECT num.stratum_1 AS num_stratum_1,
+		num.stratum_2 AS num_stratum_2,
+		num.stratum_3 AS num_stratum_3,
+		num.stratum_4 AS num_stratum_4,
 		num.count_value AS num_count_value,
 		denom.count_value AS denom_count_value
 	FROM (
@@ -28,8 +28,6 @@ FROM (
 			AND num.stratum_4 = denom.stratum_3
 	) tmp
 INNER JOIN @cdm_database_schema.concept c1
-	ON num_stratum_1 = c1.concept_id
+	ON num_stratum_1 = CAST(c1.concept_id as VARCHAR)
 INNER JOIN @cdm_database_schema.concept c2
-	ON num_stratum_3 = c2.concept_id
-ORDER BY c1.concept_id,
-	num_stratum_2
+	ON num_stratum_3 = CAST(c2.concept_id as VARCHAR)

inst/sql/sql_server/export_v5/visit/sqlPrevalenceByGenderAgeYear.sql
@@ -5,10 +5,10 @@ SELECT c1.concept_id AS concept_id,
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons
 FROM (
-	SELECT CAST(num.stratum_1 AS INT) AS num_stratum_1,
-		CAST(num.stratum_2 AS INT) AS num_stratum_2,
-		CAST(num.stratum_3 AS INT) AS num_stratum_3,
-		CAST(num.stratum_4 AS INT) AS num_stratum_4,
+	SELECT num.stratum_1 AS num_stratum_1,
+		num.stratum_2 AS num_stratum_2,
+		num.stratum_3 AS num_stratum_3,
+		num.stratum_4 AS num_stratum_4,
 		num.count_value AS num_count_value,
 		denom.count_value AS denom_count_value
 	FROM (
@@ -28,9 +28,8 @@ FROM (
 			AND num.stratum_4 = denom.stratum_3
 	) tmp
 INNER JOIN @cdm_database_schema.concept c1
-	ON num_stratum_1 = c1.concept_id
+	ON num_stratum_1 = CAST(c1.concept_id as VARCHAR)
 INNER JOIN @cdm_database_schema.concept c2
-	ON num_stratum_3 = c2.concept_id
-ORDER BY c1.concept_id,
-	num_stratum_2
+	ON num_stratum_3 = CAST(c2.concept_id as VARCHAR)
+
 

inst/sql/sql_server/export_v5/death/sqlPrevalenceByGenderAgeYear.sql
@@ -8,6 +8,6 @@ from
 	(select * from ACHILLES_results where analysis_id = 116) denom on num.stratum_1 = denom.stratum_1  --calendar year
 		and num.stratum_2 = denom.stratum_2 --gender
 		and num.stratum_3 = denom.stratum_3 --age decile
-	inner join @cdm_database_schema.concept c2 on CAST(num.stratum_2 as INT) = c2.concept_id
+	inner join @cdm_database_schema.concept c2 on num.stratum_2 = CAST(c2.concept_id as VARCHAR)
 where c2.concept_id in (8507, 8532)
-ORDER BY CAST(num.stratum_1 as INT)
+

commit 658d332ffa3cbadf9b79668f6e6e5c96447d843d

Fixed errors introduced in commit a18b3d2f45f577df7c3f18da8c1c570e4b66d310

inst/sql/sql_server/export_v5/condition/sqlPrevalenceByGenderAgeYear.sql
@@ -1,6 +1,6 @@
 SELECT c1.concept_id AS concept_id,
   c1.concept_name as concept_name,
-	cast(num_stratum_4 * 10 AS VARCHAR) + '-' + cast((num_stratum_4 + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
+	cast(CAST(num_stratum_4 AS INT) * 10 AS VARCHAR) + '-' + cast((CAST(num_stratum_4 AS INT) + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
 	c2.concept_name AS series_name,  --gender
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons

inst/sql/sql_server/export_v5/conditionera/sqlPrevalenceByGenderAgeYear.sql
@@ -1,5 +1,5 @@
 SELECT c1.concept_id AS concept_id,
-	cast(num_stratum_4 * 10 AS VARCHAR) + '-' + cast((num_stratum_4 + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
+	cast(CAST(num_stratum_4 AS INT) * 10 AS VARCHAR) + '-' + cast((CAST(num_stratum_4 AS INT) + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
 	c2.concept_name AS series_name,  --gender
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons

inst/sql/sql_server/export_v5/drug/sqlPrevalenceByGenderAgeYear.sql
@@ -1,6 +1,6 @@
 SELECT c1.concept_id AS concept_id,
 	c1.concept_name as concept_name,
-	cast(num_stratum_4 * 10 AS VARCHAR) + '-' + cast((num_stratum_4 + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
+	cast(CAST(num_stratum_4 AS INT) * 10 AS VARCHAR) + '-' + cast((CAST(num_stratum_4 AS INT) + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
 	c2.concept_name AS series_name,  --gender
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons

inst/sql/sql_server/export_v5/drugera/sqlPrevalenceByGenderAgeYear.sql
@@ -1,5 +1,5 @@
 SELECT c1.concept_id AS concept_id,
-	cast(num_stratum_4 * 10 AS VARCHAR) + '-' + cast((num_stratum_4 + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
+	cast(CAST(num_stratum_4 AS INT) * 10 AS VARCHAR) + '-' + cast((CAST(num_stratum_4 AS INT) + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
 	c2.concept_name AS series_name,  --gender
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons

inst/sql/sql_server/export_v5/observation/sqlPrevalenceByGenderAgeYear.sql
@@ -1,6 +1,6 @@
 SELECT c1.concept_id AS concept_id,
 	c1.concept_name as concept_name,
-	cast(num_stratum_4 * 10 AS VARCHAR) + '-' + cast((num_stratum_4 + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
+	cast(CAST(num_stratum_4 AS INT) * 10 AS VARCHAR) + '-' + cast((CAST(num_stratum_4 AS INT) + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
 	c2.concept_name AS series_name,  --gender
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons

inst/sql/sql_server/export_v5/procedure/sqlPrevalenceByGenderAgeYear.sql
@@ -1,6 +1,6 @@
 SELECT c1.concept_id AS concept_id,
 	c1.concept_name as concept_name,
-	cast(num_stratum_4 * 10 AS VARCHAR) + '-' + cast((num_stratum_4 + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
+	cast(CAST(num_stratum_4 AS INT) * 10 AS VARCHAR) + '-' + cast((CAST(num_stratum_4 AS INT) + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
 	c2.concept_name AS series_name,  --gender
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons

inst/sql/sql_server/export_v5/visit/sqlPrevalenceByGenderAgeYear.sql
@@ -1,6 +1,6 @@
 SELECT c1.concept_id AS concept_id,
 	c1.concept_name as concept_name,
-	cast(num_stratum_4 * 10 AS VARCHAR) + '-' + cast((num_stratum_4 + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
+	cast(CAST(num_stratum_4 AS INT) * 10 AS VARCHAR) + '-' + cast((CAST(num_stratum_4 AS INT) + 1) * 10 - 1 AS VARCHAR) AS trellis_name, --age decile
 	c2.concept_name AS series_name,  --gender
 	num_stratum_2 AS x_calendar_year, -- calendar year, note, there could be blanks
 	ROUND(1000 * (1.0 * num_count_value / denom_count_value), 5) AS y_prevalence_1000pp --prevalence, per 1000 persons

tests/testthat/test-main-achilles.R
@@ -1,7 +1,9 @@
 library(testthat)
 
-# These tests currently just check if the main achilles function doesn't throw any errors on the different platforms
+# These tests currently just check if the main achilles function and export functiosn don't throw any errors on the different platforms
 # Note: Currently only checking CDM v5
+
+
 test_that("Achilles main does not throw an error on Postgres", {
   # Postgresql
   details <- createConnectionDetails(dbms = "postgresql",