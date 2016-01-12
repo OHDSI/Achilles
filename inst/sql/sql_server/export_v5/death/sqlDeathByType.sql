ebc401d1f2e248dd067112a97daf82731092980e
Juan M. Banda <jmbanda@stanford.edu>
2016-01-12 14:15:08 -0800

    added compatibility with results_schema for exportToJson for cdmv5

--- inst/sql/sql_server/export_v5/death/sqlDeathByType.sql
+++ inst/sql/sql_server/export_v5/death/sqlDeathByType.sql
@@ -1,6 +1,6 @@
 select c2.concept_id as concept_id,
 	c2.concept_name as concept_name, 
 	ar1.count_value as count_value
-from ACHILLES_results ar1
+from @results_database_schema.ACHILLES_results ar1
 	inner join  @cdm_database_schema.concept c2 on ar1.stratum_1 = CAST(c2.concept_id as VARCHAR)
 where ar1.analysis_id = 505
