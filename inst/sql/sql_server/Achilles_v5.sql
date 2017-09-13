500c4f938acc893eca7cbb8b31521614b83bd26b
Peter Rijnbeek <p.rijnbeek@erasmusmc.nl>
2017-09-13 13:24:18 -0400

    Performance enhancements for WebAPI/Atlas (#212)

--- inst/sql/sql_server/Achilles_v5.sql
+++ inst/sql/sql_server/Achilles_v5.sql
@@ -7763,3 +7763,19 @@ where count_value <= @smallcellcount;
 delete from @results_database_schema.ACHILLES_results_dist 
 where count_value <= @smallcellcount;
 
+/**************************************************/
+/***** Add indexes to Achilles results tables *****/
+/**************************************************/
+CREATE INDEX achilles_results_analysis_id_idx
+  ON @results_database_schema.ACHILLES_results (analysis_id);
+CREATE INDEX achilles_results_stratum_1_idx
+  ON @results_database_schema.ACHILLES_results (stratum_1);
+CREATE INDEX achilles_results_stratum_2_idx
+  ON @results_database_schema.ACHILLES_results (stratum_2);
+
+CREATE INDEX achilles_results_dist_analysis_id_idx
+  ON @results_database_schema.ACHILLES_results_dist (analysis_id);
+CREATE INDEX achilles_results_dist_stratum_1_idx
+  ON @results_database_schema.ACHILLES_results_dist (stratum_1);
+CREATE INDEX achilles_results_dist_stratum_2_idx
+  ON @results_database_schema.ACHILLES_results_dist (stratum_2);
