7b8e0d5b0d4978e9168f0f0085828fc3e286142a
Vojtech Huser <vojtech.huser@gmail.com>
2018-01-24 16:40:32 -0500

    drop v4 support, documentation fixes (#242) (#243)
    * drop v4 support, documentation fixes
    
    #228 drop support for v4 in achilles() function
    #182 fixes for 3 warnings  (1 still remains)
    
    * Update Achilles.R
    
    Edited error message.

--- R/Achilles-package.R
+++ R/Achilles-package.R
@@ -4,23 +4,16 @@
 #' @docType package
 NULL
 
-#' Achilles analyses details
+
+
+
+#' allReports
 #' 
-#' A data frame describing all Achilles analyses and their IDs. The variables are as follows:
+#' A list of reports
 #'
-#' \itemize{
-#'   \item ANALYSIS_ID 
-#'   \item ANALYSIS_NAME
-#'   \item STRATUM_1_NAME
-#'   \item STRATUM_2_NAME
-#'   \item STRATUM_3_NAME
-#'   \item STRATUM_4_NAME
-#'   \item STRATUM_5_NAME
-#' }
 #'
 #' @docType data
 #' @keywords datasets
-#' @name analysesDetails
-#' @usage data(analysesDetails)
-#' @format A data frame with 7 columns
+#' @name allReports
+
 NULL
