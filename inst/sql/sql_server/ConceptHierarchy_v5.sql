9c29f9ecaeb5ea124c855a28835676615b477235
Vitaly Koulakov <vitaly.koulakov@firstlinesoftware.com>
2018-01-25 18:33:46 +0300

    ATLP-97 Impala: Migrate SQL rules for ACHILLES

--- inst/sql/sql_server/ConceptHierarchy_v5.sql
+++ inst/sql/sql_server/ConceptHierarchy_v5.sql
@@ -28,7 +28,7 @@ INSERT INTO @results_database_schema.concept_hierarchy
   SELECT
     snomed.concept_id,
     snomed.concept_name AS snomed_concept_name,
-    'Condition'         AS treemap,
+    CAST('Condition' AS VARCHAR(20)) AS treemap,
     pt_to_hlt.pt_concept_name,
     hlt_to_hlgt.hlt_concept_name,
     hlgt_to_soc.hlgt_concept_name,
@@ -129,7 +129,7 @@ INSERT INTO @results_database_schema.concept_hierarchy
   SELECT
     rxnorm.concept_id,
     rxnorm.concept_name AS rxnorm_concept_name,
-    'Drug'              AS treemap,
+    CAST('Drug' AS VARCHAR(20)) AS treemap,
     rxnorm.rxnorm_ingredient_concept_name,
     atc5_to_atc3.atc5_concept_name,
     atc3_to_atc1.atc3_concept_name,
@@ -221,7 +221,7 @@ INSERT INTO @results_database_schema.concept_hierarchy
   SELECT
     rxnorm.rxnorm_ingredient_concept_id,
     rxnorm.rxnorm_ingredient_concept_name,
-    'Drug Era'        AS treemap,
+    CAST('Drug Era' AS VARCHAR(20)) AS treemap,
     atc5_to_atc3.atc5_concept_name,
     atc3_to_atc1.atc3_concept_name,
     atc1.concept_name AS atc1_concept_name
@@ -307,10 +307,10 @@ INSERT INTO @results_database_schema.concept_hierarchy
   SELECT
     m.concept_id,
     m.concept_name,
-    'Measurement'        AS treemap,
-    max(c1.concept_name) AS level1_concept_name,
-    max(c2.concept_name) AS level2_concept_name,
-    max(c3.concept_name) AS level3_concept_name
+    CAST('Measurement' AS VARCHAR(20)) AS treemap,
+    CAST(max(c1.concept_name) AS VARCHAR(255)) AS level1_concept_name,
+    CAST(max(c2.concept_name) AS VARCHAR(255)) AS level2_concept_name,
+    CAST(max(c3.concept_name) AS VARCHAR(255)) AS level3_concept_name
   FROM
     (
       SELECT DISTINCT
@@ -336,10 +336,10 @@ INSERT INTO @results_database_schema.concept_hierarchy
   SELECT
     obs.concept_id,
     obs.concept_name,
-    'Observation'        AS treemap,
-    max(c1.concept_name) AS level1_concept_name,
-    max(c2.concept_name) AS level2_concept_name,
-    max(c3.concept_name) AS level3_concept_name
+    CAST('Observation' AS VARCHAR(20)) AS treemap,
+    CAST(max(c1.concept_name) AS VARCHAR(255)) AS level1_concept_name,
+    CAST(max(c2.concept_name) AS VARCHAR(255)) AS level2_concept_name,
+    CAST(max(c3.concept_name) AS VARCHAR(255)) AS level3_concept_name
   FROM
     (
       SELECT
@@ -364,16 +364,16 @@ INSERT INTO @results_database_schema.concept_hierarchy
 (concept_id, concept_name, treemap, level1_concept_name, level2_concept_name, level3_concept_name)
   SELECT
     procs.concept_id,
-    procs.proc_concept_name,
-    'Procedure'                          AS treemap,
-    max(proc_hierarchy.os3_concept_name) AS level2_concept_name,
-    max(proc_hierarchy.os2_concept_name) AS level3_concept_name,
-    max(proc_hierarchy.os1_concept_name) AS level4_concept_name
+    CAST(procs.proc_concept_name AS VARCHAR(400)),
+    CAST('Procedure' AS VARCHAR(20)) AS treemap,
+    CAST(max(proc_hierarchy.os3_concept_name) AS VARCHAR(255)) AS level2_concept_name,
+    CAST(max(proc_hierarchy.os2_concept_name) AS VARCHAR(255)) AS level3_concept_name,
+    CAST(max(proc_hierarchy.os1_concept_name) AS VARCHAR(255)) AS level4_concept_name
   FROM
     (
       SELECT
         c1.concept_id,
-        v1.vocabulary_name + ' ' + c1.concept_code + ': ' + c1.concept_name AS proc_concept_name
+        CONCAT(v1.vocabulary_name, ' ', c1.concept_code, ': ', c1.concept_name) AS proc_concept_name
       FROM @vocab_database_schema.concept c1
         INNER JOIN @vocab_database_schema.vocabulary v1
           ON c1.vocabulary_id = v1.vocabulary_id
