9b091bc929e6a7da36eb68623a79e46d034ff32b
Londhe, Ajit [JRDUS] <alondhe2@its.jnj.com>
2018-02-25 12:29:12 -0500

    Removed v4 files

--- inst/sql/sql_server/export_v4/observation/sqlObservationsByType.sql
+++
@@ -1,9 +0,0 @@
-select c1.concept_id as OBSERVATION_CONCEPT_ID, 
-  c1.concept_name as OBSERVATION_CONCEPT_NAME, 
-	c2.concept_id as CONCEPT_ID,
-	c2.concept_name as CONCEPT_NAME, 
-	ar1.count_value as COUNT_VALUE
-from @results_database_schema.ACHILLES_results ar1
-	inner join @vocab_database_schema.concept c1 on ar1.stratum_1 = CAST(c1.concept_id as VARCHAR)
-	inner join @vocab_database_schema.concept c2 on ar1.stratum_2 = CAST(c2.concept_id as VARCHAR)
-where ar1.analysis_id = 805
--- inst/sql/sql_server/export_v5/domainmeta/sqlDomainMeta.sql
+++
@@ -1,4 +0,0 @@
-select domain_id as AttributeName, description as AttributeValue
-from @cdm_database_schema.cdm_domain_meta
-where description is not null
-order by domain_id
