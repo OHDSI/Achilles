340eee96dc4afc1cae669d2e8af40886a1587246
Taha Abdul-Basser <ta2471@cumc.columbia.edu>
2018-07-12 18:00:38 -0400

    attempt to fix travis build error

--- inst/sql/sql_server/AchillesHeel_v5.sql
+++ inst/sql/sql_server/AchillesHeel_v5.sql
@@ -141,7 +141,7 @@ select
    CAST(100.0*count_value/statistic.total_pts AS FLOAT) as statistic_value,
    CAST(CONCAT('ach_',CAST(analysis_id as VARCHAR(10)),':Percentage') AS VARCHAR(100)) as measure_id
   from @results_database_schema.achilles_results 
-	cross join (SELECT TOP 1 count_value as total_pts from @results_database_schema.achilles_results r where analysis_id =1) as statistic
+	cross join (SELECT count_value as total_pts from @results_database_schema.achilles_results r where analysis_id =1) as statistic
   where analysis_id in (2000,2001,2002,2003);
   
 
