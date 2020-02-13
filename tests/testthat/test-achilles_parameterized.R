2f86b2e04b3239453270ef41f8cfc291ca5412bc
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2020-02-13 13:27:08 +0300

    added additional loop to cover both possible values of parameter "optimizeAtlasCache" (#1308)

--- tests/testthat/test-achilles_parameterized.R
+++ tests/testthat/test-achilles_parameterized.R
@@ -13,41 +13,43 @@ dbTypes = c("mysql",
 
 for (dbType in dbTypes) {
   for (numThreads in c(1, 3)) {
-    test_that(sprintf("Achilles main with %d threads does not throw an error on %s", numThreads, dbType), {
-      sysUser <- Sys.getenv(sprintf("CDM5_%s_USER", toupper(dbType)))
-      sysPassword <- URLdecode(Sys.getenv(sprintf("CDM5_%s_PASSWORD", toupper(dbType))))
-      sysServer <- Sys.getenv(sprintf("CDM5_%s_SERVER", toupper(dbType)))
-      sysExtraSettings <- Sys.getenv(sprintf("CDM5_%s_EXTRA_SETTINGS", toupper(dbType)))
-      if (sysUser != "" &
-          sysPassword != "" &
-          sysServer != "") {
-        cdmDatabaseSchema <- Sys.getenv(sprintf("CDM5_%s_CDM_SCHEMA", toupper(dbType)))
-        resultsDatabaseSchema <- Sys.getenv("CDM5_%s_OHDSI_SCHEMA", toupper(dbType))
+    for (optimizeAtlasCache in c(TRUE, FALSE)) {
+      test_that(sprintf("Achilles main with %d threads does not throw an error on %s", numThreads, dbType), {
+        sysUser <- Sys.getenv(sprintf("CDM5_%s_USER", toupper(dbType)))
+        sysPassword <- URLdecode(Sys.getenv(sprintf("CDM5_%s_PASSWORD", toupper(dbType))))
+        sysServer <- Sys.getenv(sprintf("CDM5_%s_SERVER", toupper(dbType)))
+        sysExtraSettings <- Sys.getenv(sprintf("CDM5_%s_EXTRA_SETTINGS", toupper(dbType)))
+        if (sysUser != "" &
+            sysPassword != "" &
+            sysServer != "") {
+          cdmDatabaseSchema <- Sys.getenv(sprintf("CDM5_%s_CDM_SCHEMA", toupper(dbType)))
+          resultsDatabaseSchema <- Sys.getenv("CDM5_%s_OHDSI_SCHEMA", toupper(dbType))
         
-        details <- createConnectionDetails(dbms = dbType,
-                                           user = sysUser,
-                                           password = sysPassword,
-                                           server = sysServer,
-                                           extraSettings = sysExtraSettings)
-        try(result <- Achilles::achilles(details, 
-                               cdmDatabaseSchema = cdmDatabaseSchema, 
-                               resultsDatabaseSchema = resultsDatabaseSchema, 
-                               scratchDatabaseSchema = resultsDatabaseSchema,
-                               sourceName = "NHANES", 
-                               cdmVersion = "5", 
-                               numThreads = numThreads, 
-                               dropScratchTables = TRUE,
-                               validateSchema = FALSE, 
-                               createTable = TRUE, 
-                               createIndices = FALSE,
-                               optimizeAtlasCache=TRUE))
-        if (file.exists("errorReport.txt")) {
-          writeLines(readChar("errorReport.txt", file.info("errorReport.txt")$size))
+          details <- createConnectionDetails(dbms = dbType,
+                                             user = sysUser,
+                                             password = sysPassword,
+                                             server = sysServer,
+                                             extraSettings = sysExtraSettings)
+          try(result <- Achilles::achilles(details,
+                                 cdmDatabaseSchema = cdmDatabaseSchema,
+                                 resultsDatabaseSchema = resultsDatabaseSchema,
+                                 scratchDatabaseSchema = resultsDatabaseSchema,
+                                 sourceName = "NHANES",
+                                 cdmVersion = "5",
+                                 numThreads = numThreads,
+                                 dropScratchTables = TRUE,
+                                 validateSchema = FALSE,
+                                 createTable = TRUE,
+                                 createIndices = FALSE,
+                                 optimizeAtlasCache=optimizeAtlasCache))
+          if (file.exists("errorReport.txt")) {
+            writeLines(readChar("errorReport.txt", file.info("errorReport.txt")$size))
+          }
+          expect_true(class(result) == "achillesResults")
+        } else {
+          writeLines(sprintf("Skipping %s main test", dbType))
         }
-        expect_true(class(result) == "achillesResults")
-      } else {
-        writeLines(sprintf("Skipping %s main test", dbType))
-      }
-    })
+      })
+    }
   }
 }
