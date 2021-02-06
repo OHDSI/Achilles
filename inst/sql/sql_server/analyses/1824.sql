186a5a4a02ce79f4f7254ea647e546a58cdb1c4a
amolin19 <amolin19@its.jnj.com>
2021-02-05 20:10:00 -0500

    first attempt at performance optimizations

--- inst/sql/sql_server/analyses/1824.sql
+++ inst/sql/sql_server/analyses/1824.sql
@@ -1,47 +1,40 @@
 -- 1824	
--- For each measurement concept, compute the top 15 co-occurring measurement concepts.
+-- For each measurement concept, find and rank the top 10 co-occurring measurement concepts.
 --
 
--- create unique person_id,measurement_concept_id pairs for people with at least 2 different (mapped) measurements.
-
---HINT DISTRIBUTE_ON_KEY(measurement_concept_id)
-select distinct person_id,measurement_concept_id
-  into #unique_pairs_1824
-  from @cdmDatabaseSchema.measurement
- where measurement_concept_id != 0
-   and person_id not in ( select person_id 
-                            from @cdmDatabaseSchema.measurement
-						   where measurement_concept_id != 0
-                           group by person_id
-                          having count(distinct measurement_concept_id) = 1 );
-
- -- Create ordered pairs of concept_ids, then count and rank them (measurement pairs must have at least 1000 distinct people) 
-
- --HINT DISTRIBUTE_ON_KEY(stratum_1)
+-- HINT DISTRIBUTE_ON_KEY(stratum_1)
+-- Find all measurement_concept_id pairs, the number of people with these pairs, 
+-- and the number of times these pairs occur.
+with pairs as (
+select m1.measurement_concept_id measurement_concept_id_1,
+       m2.measurement_concept_id measurement_concept_id_2,
+       count(distinct m2.person_id) num_people,
+       count(*) num_cases
+  from @cdmDatabaseSchema.measurement m1
+  join @cdmDatabaseSchema.measurement m2
+    on m1.person_id = m2.person_id
+ where m1.measurement_concept_id != 0
+   and m2.measurement_concept_id != 0
+   and m1.measurement_concept_id != m2.measurement_concept_id
+ group by m1.measurement_concept_id, m2.measurement_concept_id
+), ranks as (
+-- Rank order the pairs by the number of people who have them and use
+-- the number of cases as a tie breaker
+select measurement_concept_id_1, 
+       measurement_concept_id_2, 
+	   num_people,
+	   num_cases,
+       row_number()over(partition by measurement_concept_id_1 order by num_people desc, num_cases desc) ranking
+  from pairs
+) 
 select 1824 as analysis_id,
-       cast(concept_id_1 as varchar(255)) as stratum_1,
-	   cast(concept_id_2 as varchar(255)) as stratum_2,
-	   cast(ranking      as varchar(255)) as stratum_3,
-	   cast(null         as varchar(255)) as stratum_4,
-       cast(null         as varchar(255)) as stratum_5,
-	   num_people        as count_value		
-  into @scratchDatabaseSchema@schemaDelim@tempAchillesPrefix_1824
-  from (
-select concept_id_1, concept_id_2, num_people,
-       row_number()over(partition by concept_id_1 order by num_people desc) ranking
-  from (
-select t1.measurement_concept_id concept_id_1,
-       t2.measurement_concept_id concept_id_2,
-       count(distinct t1.person_id) num_people
-  from #unique_pairs_1824 t1,
-       #unique_pairs_1824 t2
- where t1.person_id               = t2.person_id
-   and t1.measurement_concept_id != t2.measurement_concept_id
- group by t1.measurement_concept_id,t2.measurement_concept_id
-having count(distinct t1.person_id) >= 1000 
-       ) tmp
-       ) tmp
- where ranking <= 15;
-
-truncate table #unique_pairs_1824;
-drop table #unique_pairs_1824;
+       cast(measurement_concept_id_1 as varchar(255)) as stratum_1,
+	   cast(measurement_concept_id_2 as varchar(255)) as stratum_2,
+	   cast(ranking                  as varchar(255)) as stratum_3,
+	   cast(num_people               as varchar(255)) as stratum_4,
+       cast(num_cases                as varchar(255)) as stratum_5,
+	   num_people                    as count_value		
+  into @scratchDatabaseSchema@schemaDelim@tempAchillesPrefix_1824 
+  from ranks 
+ where ranking <= 10
+;
--- inst/sql/sql_server/analyses/424.sql
+++ inst/sql/sql_server/analyses/424.sql
@@ -1,47 +1,42 @@
 -- 424
--- For each condition concept, compute the top 15 co-occurring condition concepts.
+-- For each condition concept, find and rank the top 10 co-occurring condition concepts.
 --
 
--- create unique person_id,condition_concept_id pairs for people with at least 2 different (mapped) conditions.
-
---HINT DISTRIBUTE_ON_KEY(condition_concept_id)
-select distinct person_id,condition_concept_id
-  into #unique_pairs_424
-  from @cdmDatabaseSchema.condition_occurrence
- where condition_concept_id != 0
-   and person_id not in ( select person_id 
-                            from @cdmDatabaseSchema.condition_occurrence
-						   where condition_concept_id != 0
-                           group by person_id
-                          having count(distinct condition_concept_id) = 1 );
-
--- Create ordered pairs of concept_ids, then count and rank them (condition pairs must have at least 1000 distinct people)
-
- --HINT DISTRIBUTE_ON_KEY(stratum_1)
+-- HINT DISTRIBUTE_ON_KEY(stratum_1)
+-- Find all condition_concept_id pairs, the number of people with these pairs, 
+-- and the number of times these pairs occur.
+with pairs as (
+select c1.condition_concept_id condition_concept_id_1,
+       c2.condition_concept_id condition_concept_id_2,
+       count(distinct c2.person_id) num_people,
+       count(*) num_cases
+  from @cdmDatabaseSchema.condition_occurrence c1
+  join @cdmDatabaseSchema.condition_occurrence c2
+    on c1.person_id = c2.person_id
+ where c1.condition_concept_id != 0
+   and c2.condition_concept_id != 0
+   and c1.condition_concept_id != c2.condition_concept_id
+ group by c1.condition_concept_id, c2.condition_concept_id
+), ranks as (
+-- Rank order the pairs by the number of people who have them and use
+-- the number of cases as a tie breaker
+select condition_concept_id_1, 
+       condition_concept_id_2, 
+	   num_people,
+	   num_cases,
+       row_number()over(partition by condition_concept_id_1 order by num_people desc, num_cases desc) ranking
+  from pairs
+) 
 select 424 as analysis_id,
-       cast(concept_id_1 as varchar(255)) as stratum_1,
-	   cast(concept_id_2 as varchar(255)) as stratum_2,
-	   cast(ranking      as varchar(255)) as stratum_3,
-	   cast(null         as varchar(255)) as stratum_4,
-       cast(null         as varchar(255)) as stratum_5,
-	   num_people        as count_value		
-  into @scratchDatabaseSchema@schemaDelim@tempAchillesPrefix_424
-  from (
-select concept_id_1, concept_id_2, num_people,
-       row_number()over(partition by concept_id_1 order by num_people desc) ranking
-  from (
-select t1.condition_concept_id concept_id_1,
-       t2.condition_concept_id concept_id_2,
-       count(distinct t1.person_id) num_people
-  from #unique_pairs_424 t1,
-       #unique_pairs_424 t2
- where t1.person_id             = t2.person_id
-   and t1.condition_concept_id != t2.condition_concept_id
- group by t1.condition_concept_id,t2.condition_concept_id
-having count(distinct t1.person_id) >= 1000 
-       ) tmp
-       ) tmp
- where ranking <= 15;
+       cast(condition_concept_id_1 as varchar(255)) as stratum_1,
+	   cast(condition_concept_id_2 as varchar(255)) as stratum_2,
+	   cast(ranking                as varchar(255)) as stratum_3,
+	   cast(num_people             as varchar(255)) as stratum_4,
+       cast(num_cases              as varchar(255)) as stratum_5,
+	   num_people                  as count_value		
+  into @scratchDatabaseSchema@schemaDelim@tempAchillesPrefix_424 
+  from ranks 
+ where ranking <= 10
+;
 
-truncate table #unique_pairs_424;
-drop table #unique_pairs_424;
+
