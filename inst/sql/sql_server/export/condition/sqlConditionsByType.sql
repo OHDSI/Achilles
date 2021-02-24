007cb4ec1f6afdd0d890259ba2db268eaae3d986
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2021-02-24 12:51:06 +0300

    Big query does not allow to use columns with aggregation functions in "GROUP BY" clause(https://github.com/OHDSI/Achilles/issues/527)

--- inst/sql/sql_server/export/condition/sqlConditionsByType.sql
+++ inst/sql/sql_server/export/condition/sqlConditionsByType.sql
@@ -1,9 +1,9 @@
 with summary as (
-  select cast(stratum_1 as int) condition_concept_id, cast(stratum_2 as int) condition_type_concept_id, sum(count_value) count_value
+  select cast(stratum_1 as int) condition_concept_id, cast(stratum_2 as int) condition_type_concept_id, count_value, sum(count_value) s_count_value
   FROM  @results_database_schema.achilles_results
   where analysis_id = 405
   GROUP BY stratum_1, stratum_2, count_value
 )
-select s.condition_concept_id, c.concept_name, s.count_value
+select s.condition_concept_id, c.concept_name, s.s_count_value as count_value
 from summary s
 join @vocab_database_schema.concept c on s.condition_type_concept_id = c.concept_id
