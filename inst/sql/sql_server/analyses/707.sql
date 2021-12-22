d3f88d41756b3334f2a2a51283c7bacb9a0b3478
AnthonyMolinaro <31700317+AnthonyMolinaro@users.noreply.github.com>
2021-12-22 12:51:05 -0500

    Closes #615

---
+++ inst/sql/sql_server/analyses/707.sql
@@ -0,0 +1,32 @@
+-- 707	Number of drug_exposure records by drug_type_concept_id
+
+--HINT DISTRIBUTE_ON_KEY(stratum_1)
+WITH rawData AS (
+SELECT 
+	de.drug_type_concept_id AS stratum_1,
+	COUNT_BIG(*)            AS count_value
+FROM 
+	@cdmDatabaseSchema.drug_exposure de
+JOIN 
+	@cdmDatabaseSchema.observation_period op 
+ON 
+	de.person_id = op.person_id
+AND 
+	de.drug_exposure_start_date >= op.observation_period_start_date
+AND 
+	de.drug_exposure_start_date <= op.observation_period_end_date
+GROUP BY 
+	de.drug_type_concept_id
+)
+SELECT
+	707 AS analysis_id,
+	CAST(stratum_1 AS VARCHAR(255)) AS stratum_1,
+	CAST(NULL AS VARCHAR(255)) AS stratum_2,
+	CAST(NULL AS VARCHAR(255)) AS stratum_3,
+	CAST(NULL AS VARCHAR(255)) AS stratum_4,
+	CAST(NULL AS VARCHAR(255)) AS stratum_5,
+	count_value
+INTO 
+	@scratchDatabaseSchema@schemaDelim@tempAchillesPrefix_707
+FROM 
+	rawData;
