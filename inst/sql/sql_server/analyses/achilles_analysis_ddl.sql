0758c8c0723ff638cfb1db2bafb9ae388ee115b4
AnthonyMolinaro <amolin19@its.jnj.com>
2022-07-27 16:26:32 -0400

    Fixes #657

---
+++ inst/sql/sql_server/analyses/achilles_analysis_ddl.sql
@@ -0,0 +1,18 @@
+
+-- DDL FOR THE ACHILLES_ANALYSIS TABLE
+
+IF OBJECT_ID('@resultsDatabaseSchema.achilles_analysis', 'U') IS NOT NULL
+  DROP TABLE @resultsDatabaseSchema.achilles_analysis;
+  
+CREATE TABLE @resultsDatabaseSchema.ACHILLES_ANALYSIS (
+	analysis_id     INTEGER,
+	analysis_name   VARCHAR(255),
+	stratum_1_name  VARCHAR(255),
+	stratum_2_name  VARCHAR(255),
+	stratum_3_name  VARCHAR(255),
+	stratum_4_name  VARCHAR(255),
+	stratum_5_name  VARCHAR(255),
+	is_default      INTEGER,
+	category        VARCHAR(255)
+);
+
