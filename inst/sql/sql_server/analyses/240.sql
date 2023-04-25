f48ec23332a8f57c36cf1aa8633da309a15fc46d
Thomas White <Thomas.M.White@MedStar.net>
2023-04-24 23:05:51 -0400

    Added person counts by {domain}_source_concept_id

---
+++ inst/sql/sql_server/analyses/240.sql
@@ -0,0 +1,26 @@
+-- 240	Number of persons with at least one visit occurrence, by visit_source_concept_id
+-- restricted to visits overlapping with observation period
+
+--HINT DISTRIBUTE_ON_KEY(stratum_1)
+SELECT 
+	240 AS analysis_id,
+	CAST(vo.visit_source_concept_id AS VARCHAR(255)) AS stratum_1,
+	CAST(NULL AS VARCHAR(255)) AS stratum_2,
+	CAST(NULL AS VARCHAR(255)) AS stratum_3,
+	CAST(NULL AS VARCHAR(255)) AS stratum_4,
+	CAST(NULL AS VARCHAR(255)) AS stratum_5,
+	COUNT_BIG(DISTINCT vo.person_id) AS count_value
+INTO 
+	@scratchDatabaseSchema@schemaDelim@tempAchillesPrefix_240
+FROM 
+	@cdmDatabaseSchema.visit_occurrence vo
+JOIN 
+	@cdmDatabaseSchema.observation_period op 
+ON 
+	vo.person_id = op.person_id
+AND 
+	vo.visit_start_date >= op.observation_period_start_date
+AND 
+	vo.visit_start_date <= op.observation_period_end_date
+GROUP BY 
+	vo.visit_source_concept_id;
